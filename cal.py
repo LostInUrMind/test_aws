7114a1f220cc3b2003895638643a87cf990919ca
nhat <nhatnmhe186348@fpt.edu.vn>
2025-01-09 08:03:26 +0700

    Test AWS python code

--- cal.py
+++ cal.py
@@ -1,28 +1,29 @@
 
-num1 = input("Enter num1 :")
-num2 = input("Enter num2 :")
-op = input("Operation: ")
+while True:
+    num1 = input("Enter num1 :")
+    num2 = input("Enter num2 :")
+    op = input("Operation: ")
+    if num1 == 'exit' or num2 == 'exit' or op == 'exit':
+        exit()
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        result = None
 
-try:
-    num1 = float(num1)
-    num2 = float(num2)
-    result = None
-
-    if op == '+':
-        result = num1 + num2
-    elif op == '-':
-        result = num1 - num2
-    elif op == '*':
-        result = num1 * num2
-    elif op == '/':
-        if num2 != 0:
-            result = num1 / num2
+        if op == '+':
+            result = num1 + num2
+        elif op == '-':
+            result = num1 - num2
+        elif op == '*':
+            result = num1 * num2
+        elif op == '/':
+            if num2 != 0:
+                result = num1 / num2
+            else:
+                print("Error: Division by zero")
         else:
-            print("Error: Division by zero")
-    else:
-        print("Error: Invalid operation")
-
-    if result is not None:
-        print(f"{num1} {op} {num2} = {result}")
-except Exception:
-    print("Error: Invalid input. Please enter a number.")
+            print("Error: Invalid operation")
+        if result is not None:
+            print(f"{num1} {op} {num2} = {result}")
+    except Exception:
+        print("Error: Invalid input. Please enter a number.")
